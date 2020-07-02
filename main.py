c1d6db5af863b3fcac81b4af8c7a85a2f9dbf7ab
yuri20198 <54778084+yuri20198@users.noreply.github.com>
2020-07-02 11:40:13 +0800

    last choice

---
+++ main.py
@@ -0,0 +1,68 @@
+from os import remove
+from os.path import join
+
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+
+from app import app
+from flowchart_recognition import flowchart
+
+ALLOWED_EXTENSIONS = {'jpg', 'png'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request', 'status': 400})
+        resp.status_code = 400
+        return resp
+
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message': 'No file selected for uploading', 'status': 400})
+        resp.status_code = 400
+        return resp
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = join(app.config['UPLOAD_FOLDER'], filename)
+        # path = secure_filename(file.filename)
+        file.save(path)
+
+        if '-p' in request.values.keys():
+            padding = int(request.values['-p'])
+        else:
+            padding = 25
+
+        if '-o' in request.values.keys():
+            offset = int(request.values['-o'])
+        else:
+            offset = 10
+
+        if '-a' in request.values.keys():
+            arrow = int(request.values['-a'])
+        else:
+            arrow = 30
+
+        nodes = flowchart(
+            filename=path, padding=padding, offset=offset, arrow=arrow, gui=False
+        )
+
+        # remove(path)
+        resp = jsonify({'message': 'File successfully uploaded', 'data': nodes, 'status': 200})
+        resp.status_code = 201
+        return resp
+
+    else:
+        resp = jsonify({'message': 'Allowed file types are jpg and png', 'status': 400})
+        resp.status_code = 400
+        return resp
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
